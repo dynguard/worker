6e3f32f67078e95a882c1e81817d4429b813bfa3
Chris Davies <cjdavies18@gmail.com>
2014-02-23 16:09:37 +0000

    Think I have a rough idea with the threads now. Next step is to make it so that either an array or hash map keeps track of which thread_id belongs to what test

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 #include <sys/syscall.h>
 #include <sys/types.h>
 
-#define MAX_THREAD_LIMIT = 20;
+#define MAX_THREAD_LIMIT 200
 
 using namespace std;
 
@@ -32,17 +32,17 @@ void *start_vm_life(void *threadarg)
 
 	cout << "I am TID: " << tid << " and thread id: " << my_data->thread_id << "\n";
 
-	if (my_data->thread_id == 1)
-	{
-		cout << "Thread: " << tid << " is exiting\n";
-		pthread_exit(NULL);
-	}
+	sleep(rand()%50);
+	cout << "Thread: " << tid <<  " with thread_id: " << my_data->thread_id << " is exiting\n";
+	pthread_exit(NULL);
+
+
 //	cout << "Going to grab information for machine: malanize-windows-xp-" << my_data->thread_id << "\n";
 
 //	test machine;
 //	machine.get_vm_status(machine_name.str());
 //	machine.print_machine_details();
-	sleep(1000);
+	sleep(10000);
 }
 
 int main()
@@ -52,27 +52,43 @@ int main()
 	int rc;
 	int no_machines = 2;
 
-	pthread_t threads[1024];
+
+	pthread_t * threads = (pthread_t*)malloc(sizeof(pthread_t)*MAX_THREAD_LIMIT);
+	cout << "Size of threads: " << sizeof(threads) << endl;
 
 	while (1)
 	{
 		cout << "Size of no_machines: " << no_machines << endl;
-		struct thread_args td[no_machines];
 
+		struct thread_args td[no_machines];
 
 		for (int i = 0; i < no_machines; i++)
 		{
 
 			//Here we want to check that thread[i] != 0 which will indicate a closed thread
 			//I also need to ensure that we keep track of threads[i] in a different array somehow
+
 			if (threads[i])
 			{
 				int ret = pthread_kill(threads[i], 0);
+
 				//if ret == 3 that is because the thread died
-				cout << "For TID: " << i << " we got Ret: " << ret << endl;
+				if (ret == 3)
+				{
+					cout << "Thread_id: " << i << " has exited!" << endl;
+					//Mark that thread_id as exited
+					pthread_detach(threads[i]);
+					threads[i] = 0;
+					//continue;
+				}
+				else
+				{
+					cout << "Thread_id: " << i << " is still running\n";
+					continue;
+				}
 			}
 
-			cout << "Spawning new thread" << "\n";
+			cout << "Spawning new thread with id: " << i << endl;
 
 			td[i].thread_id = i;
 			td[i].message = "Hello";
@@ -89,18 +105,16 @@ int main()
 			sleep(1);
 		}
 
-		cout << "The number of threads running is: " << (sizeof(threads)/sizeof(*threads)) << "\n";
-
 //		for (int i = 0; i < int((sizeof(threads)/sizeof(*threads))); i++)
 //		{
 //			//cout << "Thread at position: " << i << " contains: " << threads[i] << "\n";
 //		}
 
-		if (no_machines == 4)
+		if (no_machines == MAX_THREAD_LIMIT)
 		{
-			sleep(10000000);
-			exit(1);
+			sleep(10);
 		}
+
 
 		no_machines+=2;
 	}
